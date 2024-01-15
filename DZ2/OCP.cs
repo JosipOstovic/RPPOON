68e68f788c9aabb6982dd3f448f8d361dc90b038
JosipOstovic <124680519+JosipOstovic@users.noreply.github.com>
2024-01-15 12:56:54 +0100

    Update OCP.cs

--- DZ2/OCP.cs
+++ DZ2/OCP.cs
@@ -1,36 +1,15 @@
 using System;
 using namespace OCP {
-    public class Racun
-    {
-        public decimal Iznos { get; set; }
-
-        public decimal IzracunajPopust(decimal popust)
-        {
-            return Iznos - (Iznos * popust / 100);
+    public class Orao {
+        public string BrzinaLetenja { get; set; }
+    }
+    
+    public class BrzinaKretanja {
+        public void IspisiBrzinuLetenja(Orao orao) {
+            Console.WriteLine($"Brzina letenja orla je: {orao.BrzinaLetenja}");
         }
     }
-
-    class Program
-    {
-        static void Main()
-        {
-            Racun racun = new Racun { Iznos = 100 };
-
-            // Popust za stalne kupce
-            decimal popustZaStalneKupce = racun.IzracunajPopust(10);
-            Console.WriteLine($"Iznos s popustom za stalne kupce: {popustZaStalneKupce}");
-
-            // Popust za nove kupce
-            decimal popustZaNoveKupce = racun.IzracunajPopust(5);
-            Console.WriteLine($"Iznos s popustom za nove kupce: {popustZaNoveKupce}");
-        }   
-    }
 }
-
-//Ovaj primjer krsi OCP jer je klasa Racun otvorena za modificiranje kada god zelimo dodati novi tip popusta
-//Problem je taj sto svaki put kada zelimo dodati novu vrstu popusta, moramo mijenjati postojeci kod klase Racun
-//Ovo se moze rijesiti tako sto uvedemo sucelje npr IPopust i tu napisemo metodu IzracunajPopust
-//dok u klasi Racun uvedemo referencu na sucelje IPopust sa getterima i setterima i nakon toga definiramo metodu IzracunajCijenu
-//unutar koje mozemo preko reference pristupiti metodi IzracunajPopust i tako cemo za svakog novog kupca imati zasebnu klasu
-//unutar koje mozemo izracunati određeni popust
+//Narusava OCP jer clasa BrzinaKretanja ispisuje samo brzinu letenja orla, a ne bi radilo sa npr. brzinom trcanja psa ili macke ili nesta slicno. 
+//Bolje bi po OCP-u bilo da imamo apstrakciju zivotinja sa metodom brzina kretanja koju nasljeđuje svaka zivotinja. 
 
